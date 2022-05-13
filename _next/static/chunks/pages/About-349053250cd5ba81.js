4246bc6ff23678b88cc552732d646f310d9b4944
Kotsudes <64353627+Kotsudes@users.noreply.github.com>
2022-05-13 17:18:55 +0000

    Deploying to gh-pages from @ Kotsudes/portfolio@d987e002539d5b0c9a6f240d11558e7766cff488 🚀

---
+++ _next/static/chunks/pages/About-349053250cd5ba81.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[921],{8697:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/About",function(){return u(3773)}])}},function(n){n.O(0,[173,773,774,888,179],(function(){return _=8697,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
